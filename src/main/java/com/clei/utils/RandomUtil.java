967de35febfa119633cfff44cfa7e23e5316da50
KIyA <yueyaye@163.com>
2019-12-27 19:38:50 +0800

    1到n 里 随机 选择 m个(m < n) 个 数字。 一个列表里随机选择一部分(分散)的时候用到

--- src/main/java/com/clei/utils/RandomUtil.java
+++ src/main/java/com/clei/utils/RandomUtil.java
@@ -1,23 +1,123 @@
 package com.clei.utils;
 
-import java.util.Arrays;
-import java.util.Random;
+import java.util.*;
+import java.util.stream.Collectors;
 
 /**
  * 随机数相关工具类
  */
 public class RandomUtil {
     // 想法1
-    // 1 从1000个数字里随机选取100个
+    // 1 从1到1000里随机选取100个
 
     public static void main(String[] args) {
-        System.out.println(System.currentTimeMillis());
-        System.out.println(Arrays.toString(intArray()));
+        int[] array = intArray(1000);
+        int[] randomArray = randomArray(array,10);
+
+        System.out.println("origin : " + Arrays.toString(array));
+        System.out.println("result : " + Arrays.toString(randomArray));
+
+        System.out.println("size : " + randomArray.length);
     }
 
-    public static int[] intArray(){
+    // 从 0 到 n 里 选 num 个
+    public static int[] randomArray(int[] array,int num){
+        int length = array.length;
+
+        // 1000里选 900个 == 1000里选 100个
+        boolean other = false;
+        int[] originArray = array;
+        if(num > length / 2){
+            other = true;
+            num = length - num;
+        }
+        // 装选入的数字
+        Set<Integer> set = new HashSet<>(num,1);
+
+        // 筛选出的 set
+        outer:
+        while (set.size() < num){
+            //
+            int[] randomIntArray = randomIntArray(num * 3 / 2,0,length);
+
+            for(int i : randomIntArray){
+                set.add(array[i]);
+
+                if(num == set.size()){
+                    break outer;
+                }
+            }
+            array = getLastArray(array,set);
+        }
+
+        //
+        if(other){
+            return getLastArray(originArray,set);
+        }
+
+        return set.stream().mapToInt(i -> i).toArray();
+    }
+
+    /**
+     * array 里 的值 不在 set 里 的 凑一个array
+     * @param array
+     * @param set
+     * @return
+     */
+    private static int[] getLastArray(int[] array, Set<Integer> set) {
+        int[] newArray = new int[array.length - set.size()];
+
+        int j = 0;
+        for (int i : array){
+            if(!set.contains(i)){
+                newArray[j] = i;
+                j ++;
+            }
+        }
+
+        return newArray;
+    }
+
+    /**
+     * 获得一个长度为num 大小在origin 到 bound[左闭右开]之间的int数组
+     * @param num
+     * @param origin
+     * @param bound
+     * @return
+     */
+    public static int[] randomIntArray(int num,int origin,int bound){
         Random random = new Random();
-        // 左闭右开
-        return random.ints(1000,1,2).toArray();
+        int[] array = random.ints(num,origin,bound).toArray();
+        return array;
+    }
+
+    /**
+     * 获得一个长度为i 的 从0到i[左闭右开] 的 int数组
+     * @param i
+     * @return
+     */
+    public static int[] intArray(int i){
+        if(i < 1){
+            return null;
+        }
+        int[] array = new int[i];
+
+        for (int j = 0; j < i; j++) {
+            array[j] = j;
+        }
+        return array;
+    }
+
+    /**
+     * 获得一个长度为i 的 从0到i[左闭右开] 的 Integer 列表
+     * @param i
+     * @return
+     */
+    public static List<Integer> integerList(int i){
+        if(i < 1){
+            return null;
+        }
+        List<Integer> list = Arrays.stream(intArray(i)).boxed().collect(Collectors.toList());
+        return list;
     }
 }
