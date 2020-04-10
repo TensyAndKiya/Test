597f783dea839271119ffc8391c2b74e54f6bacb
KIyA <yueyaye@163.com>
2020-04-10 17:44:46 +0800

    日常commit

---
+++ src/main/java/com/clei/Y2020/M01/D20/NothingTest.java
@@ -0,0 +1,30 @@
+package com.clei.Y2020.M01.D20;
+
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * 得到两个月份间的所有月份
+ */
+public class NothingTest {
+    public static void main(String[] args) {
+        int st = 201912;
+        int et = 202001;
+
+        Set<Integer> dateMonth = new TreeSet<>();
+        int startYear = st / 100;
+        int endYear = et / 100;
+        int startMonth = st % 100;
+        int endMonth = et % 100;
+        for(int i = startYear, j = startMonth; (i < endYear || (i == endYear && j <= endMonth));){
+            dateMonth.add(i * 100 + j);
+            j++;
+            if(j > 12){
+                i++;
+                j = 1;
+            }
+        }
+
+        System.out.println(dateMonth);
+    }
+}
