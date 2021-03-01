080fbd2446f6a802db0c1eabb93da9c13a8536c8
KIyA <yueyaye@163.com>
2021-03-01 15:55:19 +0800

    OO设计原则复习

--- src/main/java/com/clei/Y2019/M06/D13/DesignPattern_Proxy.java
+++ src/main/java/com/clei/Y2019/M06/D13/DesignPattern_Proxy.java
@@ -10,7 +10,13 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.util.Random;
 
+/**
+ * 设计模式 代理
+ *
+ * @author KIyA
+ */
 public class DesignPattern_Proxy {
+
     public static void main(String[] args) {
         new StaticProxy().proxy();
         new CglibDynamicProxy().proxy();
@@ -20,17 +26,19 @@ public class DesignPattern_Proxy {
 
 //静态代理依赖于被代理的类,A修改之后,B可能也要跟着修改。。
 class StaticProxy {
-    void proxy(){
-        Cat c=new Cat();
-        CatLogProxy clp=new CatLogProxy(c);
-        CatTimeProxy ctp=new CatTimeProxy(clp);
+
+    void proxy() {
+        Cat c = new Cat();
+        CatLogProxy clp = new CatLogProxy(c);
+        CatTimeProxy ctp = new CatTimeProxy(clp);
         ctp.eat();
     }
 }
 
 //继承
 class CglibDynamicProxy {
-    void proxy(){
+
+    void proxy() {
         // Test
         UserDao userDao = new UserDaoMysqlImpl();
         UserDao userDaoProxy = (UserDao) new LogHandler2().getInstance(userDao);
@@ -43,6 +51,7 @@ class CglibDynamicProxy {
 
 //接口
 class JdkDynamicProxy {
+
     void proxy() {
         //ProxyFactory Test
         UserDao userDao = new UserDaoMysqlImpl();
@@ -63,44 +72,52 @@ class JdkDynamicProxy {
     }
 }
 
-interface Eatable{
+interface Eatable {
+
     void eat();
 }
 
-class Cat implements Eatable{
+class Cat implements Eatable {
+
     public void eat() {
         PrintUtil.log("猫吃老鼠！");
     }
 }
 
-class CatLogProxy implements Eatable{
+class CatLogProxy implements Eatable {
+
     private Eatable e;
+
     public CatLogProxy(Eatable e) {
         super();
-        this.e=e;
+        this.e = e;
     }
+
     public void eat() {
         PrintUtil.log("Log Start...");
         e.eat();
         try {
             Thread.sleep(new Random().nextInt(1000));
-        }catch(InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
         PrintUtil.log("Log End...");
     }
 }
 
-class CatTimeProxy implements Eatable{
+class CatTimeProxy implements Eatable {
+
     private Eatable e;
+
     public CatTimeProxy(Eatable e) {
         super();
-        this.e=e;
+        this.e = e;
     }
+
     public void eat() {
-        long start=System.currentTimeMillis();
+        long start = System.currentTimeMillis();
         e.eat();
-        long end=System.currentTimeMillis();
+        long end = System.currentTimeMillis();
         PrintUtil.log("运行时间：" + (end - start) + "毫秒！");
     }
 }
@@ -109,7 +126,7 @@ class LogHandler2 implements MethodInterceptor {
 
     private Object target;
 
-    public Object getInstance(Object target){
+    public Object getInstance(Object target) {
         this.target = target;
         //创建增强器，用来创建动态代理类
         Enhancer enhancer = new Enhancer();
@@ -121,19 +138,23 @@ class LogHandler2 implements MethodInterceptor {
     @Override
     public Object intercept(Object o, Method method, Object[] objects, MethodProxy methodProxy) throws Throwable {
         PrintUtil.log("开启日志。。。");
-        Object result = methodProxy.invokeSuper(o,objects);
+        Object result = methodProxy.invokeSuper(o, objects);
         PrintUtil.log("关闭日志。。。");
         return result;
     }
 }
 
-interface UserDao{
-    void save(int id,String name);
+interface UserDao {
+
+    void save(int id, String name);
+
     void delete(int id);
-    void update(int id,String name);
+
+    void update(int id, String name);
 }
 
-class UserDaoMysqlImpl implements UserDao{
+class UserDaoMysqlImpl implements UserDao {
+
     public void save(int id, String name) {
         PrintUtil.log("Mysql执行保存！");
     }
@@ -147,7 +168,8 @@ class UserDaoMysqlImpl implements UserDao{
     }
 }
 
-class UserDaoOracleImpl implements UserDao{
+class UserDaoOracleImpl implements UserDao {
+
     public void save(int id, String name) {
         PrintUtil.log("Oracle执行保存！");
     }
@@ -161,16 +183,17 @@ class UserDaoOracleImpl implements UserDao{
     }
 }
 
-class ProxyFactory{
+class ProxyFactory {
+
     private Object obj;
 
-    public ProxyFactory(Object obj){
+    public ProxyFactory(Object obj) {
         super();
-        this.obj=obj;
+        this.obj = obj;
     }
 
-    public Object getTransactionProxyInstance(){
-        Object proxy=Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), (tempProxy,method,args) -> {
+    public Object getTransactionProxyInstance() {
+        Object proxy = Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), (tempProxy, method, args) -> {
             PrintUtil.log("开启事务！");
             method.invoke(obj, args);
             PrintUtil.log("关闭事务！");
@@ -179,16 +202,16 @@ class ProxyFactory{
         return proxy;
     }
 
-    public Object getLogProxyInstance(){
-        Object proxy=Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), (tempProxy,method,args) -> {
+    public Object getLogProxyInstance() {
+        Object proxy = Proxy.newProxyInstance(obj.getClass().getClassLoader(), obj.getClass().getInterfaces(), (tempProxy, method, args) -> {
             String methodName = method.getName();
             //只代理save和delete类型的方法
-            if(methodName.contains("save") || methodName.contains("delete")){
+            if (methodName.contains("save") || methodName.contains("delete")) {
                 PrintUtil.log("开启日志！");
                 method.invoke(obj, args);
                 PrintUtil.log("关闭日志！");
-            }else{
-                method.invoke(obj,args);
+            } else {
+                method.invoke(obj, args);
             }
             return tempProxy;
         });
@@ -196,12 +219,13 @@ class ProxyFactory{
     }
 }
 
-class LogHandler implements InvocationHandler{
+class LogHandler implements InvocationHandler {
+
     Object target;
 
-    public LogHandler(Object target){
+    public LogHandler(Object target) {
         super();
-        this.target=target;
+        this.target = target;
     }
 
     @Override
@@ -212,7 +236,7 @@ class LogHandler implements InvocationHandler{
         return proxy;
     }
 
-    public Object getInstance(){
-        return Proxy.newProxyInstance(target.getClass().getClassLoader(),target.getClass().getInterfaces(),this);
+    public Object getInstance() {
+        return Proxy.newProxyInstance(target.getClass().getClassLoader(), target.getClass().getInterfaces(), this);
     }
 }
